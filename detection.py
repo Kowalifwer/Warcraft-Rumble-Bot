043195fc21e93b45f35b23c8481e7d9e6192a7c4
Artem Grechushkin <artem.grechushkin@mail.ru>
2023-11-27 22:24:47 +0000

    functions (e.g click) can be tested from console through sys args

--- detection.py
+++ detection.py
@@ -60,18 +60,27 @@ if __name__ == "__alt__":
     else:
         print("Template not found.")
 
-if __name__ == "__main__":
-    location = pyautogui.locateOnScreen("icons/helmet_selected.png", confidence=0.75, grayscale=True)
+def click_image(image_name):
+    location = pyautogui.locateOnScreen(f"icons/{image_name}.png", confidence=0.75, grayscale=True)
     #move mouse to location
     pyautogui.moveTo(location[0], location[1])
     #click
     pyautogui.click(location[0], location[1])
 
-    #click 100 px above
-    pyautogui.click(location[0], location[1]-100)
+if __name__ == "__main__":
+    import sys
+    # Assume the first command-line argument is the function to call
+    function_name = sys.argv[1] if len(sys.argv) > 1 else None
 
-    #sleep
-    pyautogui.sleep(1)
-    #scroll down
+    if function_name == "click_image":
+        # Call the click_image function with the provided image name (second command-line argument)
+        image_name = sys.argv[2] if len(sys.argv) > 2 else None
+        click_image(image_name)
+    else:
+        print("Invalid function name. Available functions: click_image")
+
+def scroll_down():
     pyautogui.scroll(-500) #-500 is max down scroll
-    print(location)
+
+def scroll_up():
+    pyautogui.scroll(500)
